485d5563dd4f14fd65f50588adda92b26163cb5b
Andrew Korchevskiy <37622720+Salamandero@users.noreply.github.com>
2023-01-13 18:08:01 +0200

    change  code to Hooks method

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -63,59 +63,6 @@ const ContactForm = ({ onSubmit }) => {
     </>
   );
 };
-// class ContactForm extends Component {
-//   state = {
-//     name: '',
-//     number: '',
-//   };
-
-//   handleFormChange = event => {
-//     const { name, value } = event.currentTarget;
-//     this.setState({ [name]: value });
-//   };
-//   handleSubmit = event => {
-//     event.preventDefault();
-//     this.props.onSubmit(this.state);
-//     this.reset();
-//   };
-//   reset = () => {
-//     this.setState({ name: '', number: '' });
-//   };
-
-//   render() {
-//     return (
-//       <>
-//         <Form onSubmit={this.handleSubmit}>
-//           <Label>
-//             Name
-//             <Input
-//               type="text"
-//               name="name"
-//               value={this.state.name}
-//               onChange={this.handleFormChange}
-//               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-//               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-//               required
-//             />
-//           </Label>
-//           <Label>
-//             Number
-//             <Input
-//               type="tel"
-//               name="number"
-//               value={this.state.number}
-//               onChange={this.handleFormChange}
-//               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-//               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-//               required
-//             />
-//           </Label>
-//           <Button type="submit">Add contact</Button>
-//         </Form>
-//       </>
-//     );
-//   }
-// }
 
 ContactForm.propTypes = {
   state: PropTypes.shape({
